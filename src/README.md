c181a900f113eee84cfbb0cdca39ba5e8f81d98d
Lautaro Moreno <lemoreno2002@gmail.com>
2025-01-12 15:41:40 -0300

    Proyecto CRUD con Docker configurado

---
+++ src/README.md
@@ -0,0 +1,4 @@
+## PARA USARLO
+Levantar el servicio de DOCKER con
+  docker compose up -d
+  -d para que termine el proceso y queda abierto escuchando
--- src/routes/book.router.js
+++ src/routes/book.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router()
-const Book = require('../models/book.model')
+const Book = require('../models/book.model');
+const bookModel = require('../models/book.model');
 
 // MIDDLEWARE
 // Para poder tomar UN solo libro
@@ -13,11 +14,12 @@ const getBook = async (req,res,next) => {
     })
   }
   try {
-    book = await Book.findByID(id);
-    if (!book)
+    book = await bookModel.findById(id);
+    if (!book){
       return res.status(404).json({
         message: 'El libro no fue encontrado'
     })
+    }
   } catch (err) {
     return res.status(500).json({
         message: err.message
@@ -33,21 +35,100 @@ router.get('/', async (req, res) => {
     const books = await Book.find(); // Encuentra todos los libros
     console.log('GET ALL', books);
     
-    if (books.lenght === 0)
-      res.status(204).json([])
-    res(books) // Da 200
+    if (books.lenght === 0) 
+      { res.status(204).json([])}
+    res.json(books) // Da 200
+  } catch (error) {
+    res.status(500).json({
+      message: "HOLAA " + error.message
+    })
+  }
+})
+
+// Crear un nuevo libro (recurso)
+router.post('/', async (req, res) => {
+  const { title, author, genre, publication_date } = req?.body
+  if (!title || !author || !genre || !publication_date)
+    return res.status(400).json({
+      message: 'Los campos título, autor, género y fecha son obligatorios'
+    })
+  const book = new Book(
+    {
+      title,
+      author,
+      genre,
+      publication_date
+    }
+  )
+  try {
+    const newBook = await book.save();
+    console.log(newBook);    
+    res.status(201).json(newBook)
+  } catch (error) {
+    res.status(400).json({
+      message: error.message
+    })
+  }  
+})
+
+// GET INDIDUAL
+router.get('/:id', getBook, async (req, res) => {
+  res.json(res.book)
+})
+
+router.put('/:id', getBook, async (req, res) => {
+  try {
+    const book = res.book
+    book.title = req.body.title || book.title
+    book.author = req.body.author || book.author
+    book.genre = req.body.genre || book.genre
+    book.publication_date = req.body.publication_date || book.publication_date
+
+    const updateBook = await book.save()
+    res.json(updateBook)
+  } catch (err) {
+    res.status(400).json({
+      message: err.message
+    })
+  }
+})
+
+router.patch('/:id', getBook, async (req, res) => {
+  if (!req.body.title && !req.body.author && !req.body.genre && !req.body.publication_date) {
+    res.status(400).json({
+      message: "Al menos uno de los parámetros debe estar completo: Título, Autor, Género o Fecha de publicación."
+    })
+  } else {
+    try {
+      const book = res.book
+      book.title = req.body.title || book.title
+      book.author = req.body.author || book.author
+      book.genre = req.body.genre || book.genre
+      book.publication_date = req.body.publication_date || book.publication_date
+  
+      const updateBook = await book.save()
+      res.json(updateBook)
+    } catch (err) {
+      res.status(400).json({
+        message: err.message
+      })
+    }
+  }
+})
+
+router.delete('/:id', getBook, async (req, res) => {
+  try {
+    const book = res.book;
+    await book.deleteOne({
+      _id: book._id
+    });
+    res.json({
+      message: `El libro ${book.title} fue eliminado correctamente`
+    })
   } catch (error) {
     res.status(500).json({
       message: error.message
     })
   }
 })
-
-// Crear un nuevo libro (recurso)
-router.post('/', (req, res) => {
-  const { title, author, genre, publication_date } = req?.body
-  if (!title || !author || !genre || !publication_date)
-    return res.status(400).json({
-      message: 'Los campos título, autor, género y fecha son obligatorios'
-    })  
-})
+module.exports = router
